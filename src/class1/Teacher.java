fb3395f616b474d6be35a60e2ab5abd3f525f8d8
ZarirulIslam <zarir7116@gmail.com>
2020-12-21 20:21:06 +0600

    commit

---
+++ src/class1/Teacher.java
@@ -0,0 +1,7 @@
+package class1;
+
+public class Teacher {
+String name,gender;
+int phone;
+
+}
---
+++ src/class1/teachertesting.java
@@ -0,0 +1,52 @@
+package class1;
+
+public class teachertesting {
+
+	public static void main(String[] args) {
+		Teacher teacher1=new Teacher();
+		Teacher teacher2=new Teacher();
+		Teacher teacher3=new Teacher();
+		Teacher teacher4=new Teacher();
+		Teacher teacher5=new Teacher();
+		Teacher teacher6=new Teacher();
+		teacher1.name="Anisul ";
+		teacher1.gender="Male";
+		teacher1.phone=1956754711;
+		teacher2.name="Anisul Is";
+		teacher2.gender="Male";
+		teacher2.phone=1956754711;
+		teacher3.name="Anisul Isl";
+		teacher3.gender="Male";
+		teacher3.phone=1956754711;
+		teacher4.name="Anisul Isla";
+		teacher4.gender="Male";
+		teacher4.phone=1956754711;
+		teacher5.name="Anisul Islam";
+		teacher5.gender="Male";
+		teacher5.phone=1956754711;
+		teacher6.name="Anisul Islamm";
+		teacher6.gender="Male";
+		teacher6.phone=1956754711;
+		
+		System.out.println("Name:"+teacher1.name);
+		System.out.println("Gender:"+teacher1.gender);
+		System.out.println("Phone:"+teacher1.phone);
+		System.out.println("Name:"+teacher2.name);
+		System.out.println("Gender:"+teacher2.gender);
+		System.out.println("Phone:"+teacher2.phone);
+		System.out.println("Name:"+teacher3.name);
+		System.out.println("Gender:"+teacher3.gender);
+		System.out.println("Phone:"+teacher3.phone);
+		System.out.println("Name:"+teacher4.name);
+		System.out.println("Gender:"+teacher4.gender);
+		System.out.println("Phone:"+teacher4.phone);
+		System.out.println("Name:"+teacher5.name);
+		System.out.println("Gender:"+teacher5.gender);
+		System.out.println("Phone:"+teacher5.phone);
+		System.out.println("Name:"+teacher6.name);
+		System.out.println("Gender:"+teacher6.gender);
+		System.out.println("Phone:"+teacher6.phone);
+
+	}
+
+}
